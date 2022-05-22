a177c889825f24f0219eb029856a5d560da71eac
tsmithno2 <t.smithno2@gmail.com>
2022-05-22 18:56:10 -0400

    basic set up on pages for zane and description

---
+++ DND5eCombatSim.App/Pages/WeaponDetails.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DND5eCombatSim.App.Pages
+{
+    public partial class WeaponDetails
+    {
+    }
+}
